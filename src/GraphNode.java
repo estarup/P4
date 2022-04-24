d2e554948e5f2e78aad1fe4307cb3bc92c931fb5
estarup <emil.starup@gmailgit config --global user.name estarupgit config --global user.email emil.starup@gmail.com>
2022-04-24 10:09:03 +0200

    24aprhash

--- src/GraphNode.java
+++ src/GraphNode.java
@@ -1,6 +1,9 @@
 import org.antlr.runtime.tree.ParseTree;
 import org.antlr.runtime.tree.Tree;
 
+import java.util.Hashtable;
+
 public class GraphNode {
+    public static Hashtable<String,Integer> SymbolTable = new Hashtable<String,Integer>();
 
 }
